d7e6a237d21ab7e966983981d1ff5009e956f1ef
gary <garyh46@nycstudents.net>
2026-03-16 11:46:27 -0400

    add coin

--- tool/mygame/src/main.js
+++ tool/mygame/src/main.js
@@ -3,10 +3,14 @@ import kaboom from "kaboom"
 const k = kaboom()
 
 k.loadSprite("bean", "sprites/bean.png")
-
+k.loadSprite("coin", "sprites/coin.png")
 k.add([
 	k.pos(120, 90),
 	k.sprite("bean"),
+])
+k.add([
+	k.pos(150,90),
+	k.sprite("coin"),
 ])
 
 
