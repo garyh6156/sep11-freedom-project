86cf62d52c7c1c94141af8600481192f8b83aaf8
gary <garyh46@nycstudents.net>
2026-04-19 23:07:48 -0400

    add code

--- tool/mygame/src/main.js
+++ tool/mygame/src/main.js
@@ -2,48 +2,68 @@ import kaboom from "kaboom"
 
 kaboom()
 
-loadSprite("bean", "sprites/bean.png")
+loadSprite("kid", "sprites/kid.png")
 loadSprite("money", "sprites/money.png")
+loadSprite("mom", "sprites/mom.png")
 
 const SPEED = 200
 
 const player = add([
 	pos(120, 90),
-	sprite("bean"),
-	area(),
+	sprite("kid"),
+	scale(0.4),
+	area(0.4),
 ])
 
 onKeyDown("left", () => {
 	player.move(-SPEED, 0)
 })
 
-onKeyDown("right", () => {
+onKeyDown("right", () => { // move to the right when pressing right arrow key 
 	player.move(SPEED, 0)
 })
 
-onKeyDown("up", () => {
+onKeyDown("up", () => { // move up when pressing up arrow key
 	player.move(0, -SPEED)
 })
 
-onKeyDown("down", () => {
+onKeyDown("down", () => {  // move down when pressing down arrow key 
 	player.move(0, SPEED)
 })
 
 function spawnCoin() {
     const coin = add([
-        pos(rand(width()), rand(height())), // Random position
-        sprite("money"), // Use the money sprite
+        pos(rand(width()), rand(height())), // random position
+        sprite("money"), // use the money sprite
         scale(0.5), // size it 
-		area(),
+		area(0.5),
         "coin" 
     ]);
 }
+
+
 loop(3, spawnCoin);
 
-player.onCollide("coin", (coin) => {
-	destroy(coin)
-})
 
+player.onCollide("coin", (coin) => {
+	destroy(coin)  // destory the coin after player go on the 
+})
+var speed = 100;
+function spawnNpc(){
+	const mom = add([
+		pos(rand(width()),rand(height())),
+		sprite("mom"),
+		scale(0.3),
+		area(),
+	])
+	
+ }
+ function movment(){
+	mom.action(() => {
+		mom.moveTowards(player.pos, mom.speed);
+ }
+ )}
+loop(10, spawnNpc);
 
 
 
